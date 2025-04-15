ea8dd1f28ac1229f56409e8b1f75eb7fc7d381d0
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-16 02:24:27 +0600

    another way to route

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetails = () => {
+  const navigate = useNavigate();
   const post = useLoaderData();
   return (
     <div>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      <button onClick={()=>navigate(-1)}>Go back</button>
     </div>
   );
 };
